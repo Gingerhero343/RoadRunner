c42281cb69aa345d5519ca10660296ed2a68b3cf
Santiago Demaree <augie1700@gmail.com>
2017-01-04 19:54:36 -0600

    Test

--- RoadRunner/MainActivity.cs
+++ RoadRunner/MainActivity.cs
@@ -10,7 +10,6 @@ namespace RoadRunner
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
         }
